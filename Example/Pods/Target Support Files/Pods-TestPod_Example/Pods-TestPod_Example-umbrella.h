ea0fba1d32d33885ca84577fc3d1b908097d8bb7
Purushottam Chandra <purushottam.chandra@kuliza.com>
2017-03-07 14:05:32 +0530

    New commit

---
+++ Example/Pods/Target Support Files/Pods-TestPod_Example/Pods-TestPod_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_TestPod_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TestPod_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/TestPod/TestPod-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double TestPodVersionNumber;
+FOUNDATION_EXPORT const unsigned char TestPodVersionString[];
+
